b97b0324383a24b9cea364890756e81c45a15876
astrawnuts <astrawnuts@gmail.com>
2021-12-11 20:35:02 +1300

    first commit

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("S:\\Files\\VS_Code\\Projects\\docs_v3\\my-website\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("S:\\Files\\VS_Code\\Projects\\docs_v3\\my-website\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("S:\\Files\\VS_Code\\Projects\\docs_v3\\my-website\\node_modules\\@docusaurus\\theme-classic\\lib\\admonitions.css"),
+  require("S:\\Files\\VS_Code\\Projects\\docs_v3\\my-website\\src\\css\\custom.css"),
+];
